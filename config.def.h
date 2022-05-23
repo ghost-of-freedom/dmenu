a53ee87735c9416ec3e6195fe86d1e1ed279f5b0
ghost-of-freedom <no@example.com>
2022-05-23 12:05:09 +0200

    Apply solarized-light patch

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#657b83", "#fdf6e3" },
+	[SchemeSel] = { "#657b83", "#eee8d5"},
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
